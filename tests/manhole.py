2b7e18f3d759f6e8ad9eb5ca92fafc51a3d9d29d
rhn <gihu.rhn@porcupinefactory.org>
2018-10-22 20:30:02 +0000

    Fixed installation

--- tests/manhole.py
+++ tests/manhole.py
@@ -1,18 +1,41 @@
+from contextlib import contextmanager
+from tempfile import TemporaryDirectory
+from pathlib import Path
+
 import test
+
+
+@contextmanager
+def vm(repo_path, cleanup=True):
+    with open(repo_path.joinpath('tests/ansible_inventory.ini')) as inv:
+        inv_contents = inv.read()
+
+    with TemporaryDirectory() as td:
+        test.setup_sdist(repo_path, dest_path=Path(td))
+        sdist_path = Path(td).joinpath('sdist')
+        inv_path = Path(td).joinpath('ansible_inventory.ini')
+        with open(inv_path, 'w') as inv:
+            inv.write(inv_contents.format(dist_path=sdist_path))
+
+        try:
+            test.vagrant('up', cwd=repo_path.joinpath('tests')) # may leave an unprovisioned VM
+            test.ansible('ansible/fresh.yml', inv_path, cwd=repo_path)
+            test.ansible('ansible/hosting.yml', inv_path, cwd=repo_path)
+            test.ansible('ansible/site2.yml', inv_path, cwd=repo_path)
+            yield
+        finally:
+            if cleanup:
+                test.vagrant('destroy', cwd=repo_path.joinpath('tests'))
+
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument('stage', nargs='?', default='marginalia',
-        choices=('user', 'nginx', 'marginalia'),
-        help='Stage to which the VM should be installed')
     parser.add_argument('--keep', action='store_true', help='Keep the VM if creation fails')
     args = parser.parse_args()
 
-    stage_enum = test.VMStage.from_string(args.stage)
-    print("Creating Marginalia VM up to the stage {}".format(stage_enum))
     try:
-        with test.vm(test.REPO_PATH, install_stage=stage_enum, cleanup=not args.keep):
+        with vm(test.REPO_PATH, cleanup=not args.keep):
             input("Marginalia test VM started on 192.168.121.140. The username is deploy. Press enter to destroy it.")
     finally:
         if args.keep:
--- tests/test.py
+++ tests/test.py
@@ -3,47 +3,61 @@ from pathlib import Path
 import subprocess
 from contextlib import contextmanager
 from enum import IntEnum
+from tempfile import TemporaryDirectory
+from collections import namedtuple
 
-REPO_PATH = Path(__file__).parent.parent
+REPO_PATH = Path(__file__).absolute().parent.parent
 
-def run_ansible(repo_path, playbook_name):
-    subprocess.run(['ansible-playbook',
-            '-i', repo_path.joinpath('tests/ansible_inventory.ini'),
-            repo_path.joinpath(playbook_name)],
+
+class TestError(Exception): pass
+
+def assert_file(path):
+    if not path.is_file():
+        raise TestError("{} is not a file".format(path))
+
+
+def ansible(playbook_path, inventory_path, cwd):
+    subprocess.run(['ansible-playbook', playbook_path,
+            '-i', inventory_path],
+        cwd=cwd,
         check=True)
 
-def run_vagrant(repo_path, command):
-    subprocess.run(['vagrant', command], cwd=repo_path.joinpath('tests'), check=True)
+def vagrant(command, cwd):
+    subprocess.run(['vagrant', command], cwd=cwd, check=True)
 
-class VMStage(IntEnum):
-    user = 1
-    nginx = 2
-    marginalia = 3
+def setup_sdist(script_dir, dest_path):
+    subprocess.run(['python3', script_dir.joinpath('setup.py'), 'sdist',
+            '--dist-dir', dest_path.joinpath('sdist')],
+        cwd=script_dir, check=True)
 
-    @classmethod
-    def from_string(cls, s):
-        for name, member in cls.__members__.items():
-            if s == name:
-                return member
-        raise ValueError("No element {} in {}".format(s, cls))
+def test_install(ctx):
+    setup_sdist(ctx.repo_path, dest_path=ctx.tempdir)
+    with open(ctx.repo_path.joinpath('tests/ansible_inventory.ini')) as inv:
+        inv_contents = inv.read()
+    inv_path = ctx.tempdir.joinpath('ansible_inventory.ini')
+    with open(inv_path, 'w') as inv:
+        inv.write(inv_contents.format(dist_path=ctx.tempdir.joinpath('sdist')))
+
+    try:
+        vagrant('up', cwd=ctx.repo_path.joinpath('tests')) # may leave an unprovisioned VM
+        ansible('ansible/fresh.yml', inv_path, cwd=ctx.repo_path)
+        ansible('ansible/hosting.yml', inv_path, cwd=ctx.repo_path)
+        ansible('ansible/site2.yml', inv_path, cwd=ctx.repo_path)
+    finally:
+        vagrant('destroy', cwd=ctx.repo_path.joinpath('tests'))
+
+def test_sdist(ctx):
+    setup_sdist(ctx.repo_path, dest_path=ctx.tempdir)
+    assert_file(ctx.tempdir.joinpath('sdist/Marginalia-0.1.tar.gz'))
+
+class TestContext(namedtuple('TestContext', ['tempdir', 'repo_path'])): pass
 
 @contextmanager
-def vm(repo_path, install_stage, cleanup=True):
-    try:
-        run_vagrant(repo_path, 'up') # may leave an unprovisioned VM
-        run_ansible(repo_path, 'ansible/fresh.yml')
-        if install_stage > VMStage.user:
-            run_ansible(repo_path, 'ansible/hosting.yml')
-            if install_stage > VMStage.nginx:
-                run_ansible(repo_path, 'ansible/site2.yml')
-        yield
-    finally:
-        if cleanup:
-            run_vagrant(repo_path, 'destroy')
-
-def test_install(repo_path):
-    with vm(repo_path, VMStage.marginalia):
-        pass
+def get_test_context(repo_path):
+    with TemporaryDirectory() as d:
+        yield TestContext(Path(d), repo_path)
 
 if __name__ == '__main__':
-    test_install(REPO_PATH)
+    for test in [test_sdist, test_install]:
+        with get_test_context(REPO_PATH) as ctx:
+            test(ctx)
