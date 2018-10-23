8adfbb122be044189ad014dab20a50b52410af1a
rhn <gihu.rhn@porcupinefactory.org>
2018-10-23 15:57:19 +0000

    API works

--- tests/manhole.py
+++ tests/manhole.py
@@ -1,31 +1,5 @@
-from contextlib import contextmanager
-from tempfile import TemporaryDirectory
-from pathlib import Path
-
 import test
-
-
-@contextmanager
-def vm(repo_path, cleanup=True):
-    with open(repo_path.joinpath('tests/ansible_inventory.ini')) as inv:
-        inv_contents = inv.read()
-
-    with TemporaryDirectory() as td:
-        test.setup_sdist(repo_path, dest_path=Path(td))
-        sdist_path = Path(td).joinpath('sdist')
-        inv_path = Path(td).joinpath('ansible_inventory.ini')
-        with open(inv_path, 'w') as inv:
-            inv.write(inv_contents.format(dist_path=sdist_path))
-
-        try:
-            test.vagrant('up', cwd=repo_path.joinpath('tests')) # may leave an unprovisioned VM
-            test.ansible('ansible/fresh.yml', inv_path, cwd=repo_path)
-            test.ansible('ansible/hosting.yml', inv_path, cwd=repo_path)
-            test.ansible('ansible/site2.yml', inv_path, cwd=repo_path)
-            yield
-        finally:
-            if cleanup:
-                test.vagrant('destroy', cwd=repo_path.joinpath('tests'))
+from lib import vm
 
 
 if __name__ == '__main__':
--- tests/test.py
+++ tests/test.py
@@ -2,9 +2,12 @@
 from pathlib import Path
 import subprocess
 from contextlib import contextmanager
-from enum import IntEnum
 from tempfile import TemporaryDirectory
+import urllib.request
 from collections import namedtuple
+
+import lib
+from lib import vm, ansible, vagrant
 
 REPO_PATH = Path(__file__).absolute().parent.parent
 
@@ -15,49 +18,86 @@ def assert_file(path):
     if not path.is_file():
         raise TestError("{} is not a file".format(path))
 
+def assert_eq(a, b):
+    if not a == b:
+        raise TestError("{!r} is not equal to {!r}".format(a, b))
 
-def ansible(playbook_path, inventory_path, cwd):
-    subprocess.run(['ansible-playbook', playbook_path,
-            '-i', inventory_path],
-        cwd=cwd,
-        check=True)
+def test_html_static(ctx):
+    resp = urllib.request.urlopen('http://{addr}/index.html'.format(addr=ctx['vm_addr']))
+    assert_eq(resp.status, 200)
+    # FIXME: add test to use dummy site and compare content
 
-def vagrant(command, cwd):
-    subprocess.run(['vagrant', command], cwd=cwd, check=True)
+def test_api(ctx):
+    resp = urllib.request.urlopen('http://{addr}/api/'.format(addr=ctx['vm_addr']))
+    assert_eq(resp.status, 400)
 
-def setup_sdist(script_dir, dest_path):
-    subprocess.run(['python3', script_dir.joinpath('setup.py'), 'sdist',
-            '--dist-dir', dest_path.joinpath('sdist')],
-        cwd=script_dir, check=True)
+@contextmanager
+def vm_ctx(ctx):
+    with vm(ctx['repo_path']):
+        c2 = dict(ctx) 
+        c2.update({'vm_addr': '192.168.121.140'})
+        yield c2
 
 def test_install(ctx):
-    setup_sdist(ctx.repo_path, dest_path=ctx.tempdir)
-    with open(ctx.repo_path.joinpath('tests/ansible_inventory.ini')) as inv:
+    with open(ctx['repo_path'].joinpath('tests/ansible_inventory.ini')) as inv:
         inv_contents = inv.read()
-    inv_path = ctx.tempdir.joinpath('ansible_inventory.ini')
+    inv_path = ctx['tempdir'].joinpath('ansible_inventory.ini')
     with open(inv_path, 'w') as inv:
-        inv.write(inv_contents.format(dist_path=ctx.tempdir.joinpath('sdist')))
+        inv.write(inv_contents.format(dist_path=ctx['tempdir'].joinpath('sdist')))
 
     try:
-        vagrant('up', cwd=ctx.repo_path.joinpath('tests')) # may leave an unprovisioned VM
-        ansible('ansible/fresh.yml', inv_path, cwd=ctx.repo_path)
-        ansible('ansible/hosting.yml', inv_path, cwd=ctx.repo_path)
-        ansible('ansible/site2.yml', inv_path, cwd=ctx.repo_path)
+        vagrant('up', cwd=ctx['repo_path'].joinpath('tests')) # may leave an unprovisioned VM
+        ansible('ansible/fresh.yml', inv_path, cwd=ctx['repo_path'])
+        ansible('ansible/hosting.yml', inv_path, cwd=ctx['repo_path'])
+        ansible('ansible/site2.yml', inv_path, cwd=ctx['repo_path'])
     finally:
-        vagrant('destroy', cwd=ctx.repo_path.joinpath('tests'))
+        vagrant('destroy', cwd=ctx['repo_path'].joinpath('tests'))
+
+@contextmanager
+def sdist_ctx(ctx):
+    lib.setup_sdist(ctx['repo_path'], ctx['tempdir'])
+    yield ctx
 
 def test_sdist(ctx):
-    setup_sdist(ctx.repo_path, dest_path=ctx.tempdir)
-    assert_file(ctx.tempdir.joinpath('sdist/Marginalia-0.1.tar.gz'))
+    subprocess.run(['python3', 'setup.py', 'sdist',
+            '--dist-dir', ctx['tempdir'].joinpath('sdist')],
+        cwd=ctx['repo_path'], check=True)
+    assert_file(ctx['tempdir'].joinpath('sdist/Marginalia-0.1.tar.gz'))
 
-class TestContext(namedtuple('TestContext', ['tempdir', 'repo_path'])): pass
+@contextmanager
+def standard_ctx(ctx):
+    yield ctx
 
 @contextmanager
 def get_test_context(repo_path):
     with TemporaryDirectory() as d:
-        yield TestContext(Path(d), repo_path)
+        yield {'tempdir': Path(d),
+               'repo_path': repo_path}
+
+Suite = namedtuple('Suite', ['ctxmgr', 'tests'])
+
+suites = (Suite(standard_ctx, [test_sdist]),
+          Suite(sdist_ctx, [test_install]),
+          Suite(vm_ctx, (test_html_static, test_api)))
+
+def find_tests(suites, name):
+    def find_suite_tests(tests, name):
+        return [test for test in tests if test.__name__ == name]
+
+    for setup_mgr, tests in suites:
+        found_tests = find_suite_tests(tests, name)
+        if found_tests:
+            yield Suite(setup_mgr, found_tests)
 
 if __name__ == '__main__':
-    for test in [test_sdist, test_install]:
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('match', nargs='?', help='match test name')
+    args = parser.parse_args()
+    if args.match:
+        suites = find_tests(tests, args.match)
+    for setup_mgr, tests in suites:
         with get_test_context(REPO_PATH) as ctx:
-            test(ctx)
+            with setup_mgr(ctx) as ctx:
+                for test in tests:
+                    test(ctx)
